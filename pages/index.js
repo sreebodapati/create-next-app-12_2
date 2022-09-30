74f02fe2555a2b1f7a596017ca48c49949424f3c
sreebodapati <sreebodapati@gmail.com>
2022-09-30 17:29:02 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to fdingler!
+          Welcome to Sree Bodapati!
         </h1>
 
         <p className={styles.description}>
